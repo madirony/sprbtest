fdaa45c0e7ac33db8ce75109d675ef0b9c8c3e84
madirony <madirony52@naver.com>
2021-01-19 17:01:14 +0900

    Naver Google Login

--- src/main/java/org/example/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/org/example/springboot/config/auth/dto/SessionUser.java
@@ -5,12 +5,12 @@ import org.example.springboot.domain.user.User;
 import java.io.Serializable;
 
 @Getter
-public class SessionUser implements Serializable {
+public class SessionUser implements Serializable{
     private String name;
     private String email;
     private String picture;
 
-    public SessionUser(User user) {
+    public SessionUser(User user){
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
